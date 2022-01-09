9fa97584082ed48e876a75f7481240bc3c46cf1f
lee00777 <lee00777@algonquinlive.com>
2022-01-09 13:57:04 -0500

    Initial Commit

---
+++ githubFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubFinder
+//
+//  Created by GYUYOUNG LEE on 2022/01/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubFinder/githubFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubFinderApp.swift
+//  githubFinder
+//
+//  Created by GYUYOUNG LEE on 2022/01/09.
+//
+
+import SwiftUI
+
+@main
+struct githubFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
